c0e86d30230f81364ac51506e58c065f7c3d3e12
sanchezju <sjuan634@yahoo.com>
2022-11-21 18:27:42 -0800

    update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html', './public/assets/js/script.js}'],
+  content: ['./index.html', './assets/js/script.js'],
   theme: {
     extend: {},
   },
